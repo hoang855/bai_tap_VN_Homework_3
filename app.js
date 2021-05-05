eee38ab8c9303e5485a3be2ceb010737bed2217c
DESKTOP-KJTM1HI\Nguyễn_Hoàng <nguyenhoang555888@gmail.com>
2021-05-05 17:21:40 +0700

    bai_tap_3

---
+++ app.js
@@ -0,0 +1,9 @@
+
+
+function check(){
+    document.getElementById("check_fom_app").style.display = "block"
+}
+
+function check_1(){
+    document.getElementById("check_fom_app").style.display = "none"
+}
